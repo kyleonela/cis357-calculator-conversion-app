b453893ff6942b7742c3e5cfa129825130467627
Ben Townsend <townsenb@mail.gvsu.edu>
2020-02-06 16:26:15 -0500

    merge

---
+++ DecimalMinusDemo/DecimalMinusDemo/ViewControllerVolume.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DecimalMinusDemo
+//
+//  Created by Jonathan Engelsma on 9/25/15.
+//  Copyright (c) 2015 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerVolume: UIViewController {
+    
+    @IBOutlet weak var gallonsField: UITextField!
+    @IBOutlet weak var litersField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
